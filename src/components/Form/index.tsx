5ec557f854a8dce0cf0a4ee3a457c8334365883a
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-08-24 17:55:28 -0300

    Criação da validação de campos

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,43 +1,75 @@
+import { useState } from 'react';
+
 type FormProps = {
   onCancel: () => void;
 };
 
 function Form({ onCancel }: FormProps) {
+  const [formData, setFormData] = useState({
+    serviceName: '',
+    login: '',
+    password: '',
+    url: '',
+  });
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  }
+
+  const hasLength = formData.password.length >= 8 && formData.password.length <= 16;
+  const hasNumbers = /\d/.test(formData.password);
+  const hasLetters = /[a-zA-Z]/.test(formData.password);
+  const hasSpecialCharacters = (
+    /[!@#$%^&*(),.?":{}|<>]/.test(formData.password)
+  );
+
+  const isFormValid = formData.serviceName
+  && formData.login
+  && hasLength
+  && hasNumbers
+  && hasLetters
+  && hasSpecialCharacters;
+
   return (
     <form>
       <label>
         Nome do serviço
         <input
           type="text"
-          name=""
-          id=""
+          name="serviceName"
+          value={ formData.serviceName }
+          onChange={ handleInputChange }
         />
       </label>
       <label>
         Login
         <input
           type="text"
-          name=""
-          id=""
+          name="login"
+          value={ formData.login }
+          onChange={ handleInputChange }
         />
       </label>
       <label>
         Senha
         <input
           type="password"
-          name=""
-          id=""
+          name="password"
+          value={ formData.password }
+          onChange={ handleInputChange }
         />
       </label>
       <label>
         URL
         <input
           type="text"
-          name=""
-          id=""
+          name="url"
+          value={ formData.url }
+          onChange={ handleInputChange }
         />
       </label>
-      <button type="submit">Cadastrar</button>
+      <button type="submit" disabled={ !isFormValid }>Cadastrar</button>
       <button type="button" onClick={ onCancel }>Cancelar</button>
     </form>
   );
